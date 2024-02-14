4f38e14d92649039cc809ac021fe9cfd8adf21ac
kaarup <vares.kaarup@gmail.com>
2024-02-14 11:02:51 +0200

    timer changes

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-var pomodroTime = 25 * 60;
+var pomodoroTime = 15;
+var isTimerRunning = false;
+var downloadTimer;
 
 function fmtMSS(s) {
   return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s;
 }
 
 document.getElementById("timer_start").addEventListener("click", function () {
-  var downloadTimer = setInterval(function function1() {
-    document.getElementById("countdown").innerHTML =
-      fmtMSS(pomodroTime) + " seconds remaining";
+  if (!isTimerRunning) {
+    startTimer();
+  } else {
+    pauseTimer();
+  }
+});
 
-    pomodroTime -= 1;
-    if (pomodroTime <= 0) {
+function startTimer() {
+  isTimerRunning = true;
+  document.getElementById("timer_start").value = "Pause";
+  downloadTimer = setInterval(function () {
+    document.getElementById("countdown").innerHTML = fmtMSS(pomodoroTime);
+    pomodoroTime -= 1;
+    if (pomodoroTime <= 0) {
       clearInterval(downloadTimer);
-      document.getElementById("countdown").innerHTML = "Time is up!";
+      document.getElementById("countdown").innerHTML = "Take a break";
+      isTimerRunning = false;
+      document.getElementById("timer_start").value = "Start";
     }
   }, 1000);
-});
+}
+
+function pauseTimer() {
+  clearInterval(downloadTimer);
+  isTimerRunning = false;
+  document.getElementById("timer_start").value = "Start";
+}
